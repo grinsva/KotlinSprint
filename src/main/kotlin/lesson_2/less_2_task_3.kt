bc0b85389942e1686b4e848a2e58640fdea01a9a
grinsva <grinsva@gmail.com>
2024-06-07 12:03:20 +0300

    KS 2-3 изменил Константу добавил К, убрал входные строковые

--- src/main/kotlin/lesson_2/less_2_task_3.kt
+++ src/main/kotlin/lesson_2/less_2_task_3.kt
@@ -1,28 +1,29 @@
 package lesson_2
 
-const val minInHour = 60
+const val MIN_IN_HOUR = 60
+const val HOUR_IN_DAY = 24
 
 fun main() {
 
-    val departHourStr: String = "09"
-    val departMinStr = "39"
+    val departHour = 9
+    val departMinite = 39
     val rideTimeMinTotal = 1457
 
-    val departHourInt = departHourStr.toInt()
-    val departMinInt = departMinStr.toInt()
-    val departTotalMin = departMinInt + departHourInt * minInHour //Сколько минут от 00:00 (обычно в секундх считается)
+    //Сколько минут от 00:00 начинается отправление
+    val departTotalMin = departMinite + departHour * MIN_IN_HOUR
 
+    //Сколько минут от 00:00 прибытие
     val arriveTotalMinInt = departTotalMin + rideTimeMinTotal
 
-    val arriveHourInt  = when {
-        (arriveTotalMinInt / minInHour < 24) -> arriveTotalMinInt / minInHour
-        else -> (arriveTotalMinInt / minInHour - 24)
+    val arriveHourInt = when {
+        (arriveTotalMinInt / MIN_IN_HOUR < HOUR_IN_DAY) -> arriveTotalMinInt / MIN_IN_HOUR
+        else -> (arriveTotalMinInt / MIN_IN_HOUR - HOUR_IN_DAY)
     }
 
-    val arriveMinInt  = arriveTotalMinInt % minInHour
+    val arriveMinInt = arriveTotalMinInt % MIN_IN_HOUR
 
-    val arriveHourStr = String.format("%02d",arriveHourInt)
-    val arriveMinStr = String.format("%02d",arriveMinInt)
+    val arriveHourStr = String.format("%02d", arriveHourInt)
+    val arriveMinStr = String.format("%02d", arriveMinInt)
 
     println("Поезд прибывает в $arriveHourStr:$arriveMinStr")
 
