3fd4f13887ca60db23643aeb405dc462609862f9
grinsva <grinsva@gmail.com>
2024-06-07 13:21:04 +0300

    KS 3-3 was done

---
+++ src/main/kotlin/lesson_3/less_3_task_3.kt
@@ -0,0 +1,18 @@
+package lesson_3
+
+fun main() {
+
+    val inputNumber: Int = 2
+
+    println("$inputNumber X 1 = ${inputNumber * 1} \n" +
+            "$inputNumber X 2 = ${inputNumber * 2} \n" +
+            "$inputNumber X 3 = ${inputNumber * 3} \n" +
+            "$inputNumber X 4 = ${inputNumber * 4} \n" +
+            "$inputNumber X 5 = ${inputNumber * 5} \n" +
+            "$inputNumber X 6 = ${inputNumber * 6} \n" +
+            "$inputNumber X 7 = ${inputNumber * 7} \n" +
+            "$inputNumber X 8 = ${inputNumber * 8} \n" +
+            "$inputNumber X 9 = ${inputNumber * 9} \n"
+    )
+
+}
