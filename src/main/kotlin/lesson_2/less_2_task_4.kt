f9f84e70058350f92685849148c123a5d12e2461
grinsva <grinsva@gmail.com>
2024-06-08 12:07:19 +0300

    KS 2-4 введена константа 100%; удалён пакет lesson 1 c с файлами 1-1,2,3,4

--- src/main/kotlin/lesson_2/less_2_task_4.kt
+++ src/main/kotlin/lesson_2/less_2_task_4.kt
@@ -1,5 +1,7 @@
 package lesson_2
 
+const val PROCENT_100 = 100
+
 fun main() {
 
     val kristRuda = 27
@@ -7,8 +9,8 @@ fun main() {
 
     val baffProcent = 20
 
-    val kristRudaBonus = kristRuda * baffProcent / 100
-    val ferrRudaBonus = ferrRuda * baffProcent / 100
+    val kristRudaBonus = kristRuda * baffProcent / PROCENT_100
+    val ferrRudaBonus = ferrRuda * baffProcent / PROCENT_100
 
     println(
         "Ваш заработанный бонус: \n" +
