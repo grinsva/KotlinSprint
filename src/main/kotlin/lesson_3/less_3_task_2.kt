fb6e78bbaa7846dfb36730e8e61681173867e505
grinsva <grinsva@gmail.com>
2024-06-10 15:52:36 +0300

    KS 3-2 убрал константы

--- src/main/kotlin/lesson_3/less_3_task_2.kt
+++ src/main/kotlin/lesson_3/less_3_task_2.kt
@@ -1,35 +1,25 @@
 package lesson_3
 
-const val USER_NAME_ID_20 = "Татьяна Сергеевна"
-const val USER_SURNAME_ID_20 = "Андреева"
-const val USER_NAME_ID_22 = "Татьяна Сергеевна"
-const val USER_SURNAME_ID_22 = "Сидорова"
-
 fun main() {
 
-    val userAge = 22
+    val userAge = 21
 
-    val userName = when (userAge) {
-        20 -> USER_NAME_ID_20
-        22 -> USER_NAME_ID_22
-        else -> "нет данных об имени"
-    }
-    val userSurName = when (userAge) {
-        20 -> USER_SURNAME_ID_20
-        22 -> USER_SURNAME_ID_22
-        else -> "нет данных о фамилии"
+    val userName = "Татьяна"
+    val userFatherName = "Сергеевна"
+
+    val userSurName = when {
+        (userAge < 22) -> "Андреева"
+        else -> "Сидорова"
     }
 
-    println("Персональные данные: \n" +
-            "ФИО: $userSurName $userName\n" +
-            "Возраст: $userAge"
+    println(
+        "Персональные данные: \n" +
+                "Фамилия: $userSurName \n" +
+                "Имя: $userName \n" +
+                "Отчество: $userFatherName \n" +
+                "Возраст: $userAge"
 
     )
-
-
-
-
-
 
 
 }
