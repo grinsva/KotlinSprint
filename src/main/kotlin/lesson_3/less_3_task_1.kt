15271b6162079d8413330bfbf5f52152afe2a61a
grinsva <grinsva@gmail.com>
2024-06-10 15:40:36 +0300

    KS 3-1 ЗАМЕНИЛ МАГИЧЕСКИЕ ЦИФРЫ НА КОНСТАНТЫ

--- src/main/kotlin/lesson_3/less_3_task_1.kt
+++ src/main/kotlin/lesson_3/less_3_task_1.kt
@@ -4,15 +4,20 @@ const val GRITING_MORNING = "Доброе утро!"
 const val GRITING_EVENING = "Доброго вечера!"
 const val GRITING_DAY = "Доброго дня!"
 const val GRITING_NIGHT = "Доброй ночи!"
+
+const val MORNING_START_HOUR = 6
+const val DAY_START_HOUR = 11
+const val EVENING_START_HOUR = 18
+const val NIGHT_START_HOUR = 22
 fun main() {
 
     val userName = "Василий"
-    val hourOfDay = 7
+    val hourOfDay = 23
 
     val partOfDayGritting: String = when(hourOfDay) {
-        in 6..10 -> GRITING_MORNING
-        in 11..17 -> GRITING_DAY
-        in 18..21 -> GRITING_EVENING
+        in MORNING_START_HOUR..< DAY_START_HOUR -> GRITING_MORNING
+        in DAY_START_HOUR..< EVENING_START_HOUR -> GRITING_DAY
+        in EVENING_START_HOUR..< NIGHT_START_HOUR -> GRITING_EVENING
         else -> GRITING_NIGHT
     }
 
