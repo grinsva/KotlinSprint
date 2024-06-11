165aa7a0ee3d1939d20225adbfa8ce4cbba76b3e
grinsva <grinsva@gmail.com>
2024-06-11 11:28:27 +0300

    KS 3-2 можно больше конкретики по ТЗ?

--- src/main/kotlin/lesson_3/less_3_task_2.kt
+++ src/main/kotlin/lesson_3/less_3_task_2.kt
@@ -2,12 +2,12 @@ package lesson_3
 
 fun main() {
 
-    val userAge = 21
+    var userAge = 21
 
     val userName = "Татьяна"
     val userFatherName = "Сергеевна"
 
-    val userSurName = when {
+    var userSurName = when {
         (userAge < 22) -> "Андреева"
         else -> "Сидорова"
     }
@@ -17,9 +17,29 @@ fun main() {
                 "Фамилия: $userSurName \n" +
                 "Имя: $userName \n" +
                 "Отчество: $userFatherName \n" +
-                "Возраст: $userAge"
+                "Возраст: $userAge \n"
 
     )
+
+    userAge = 22
+
+    userSurName = when {
+        (userAge < 22) -> "Андреева"
+        else -> "Сидорова"
+    }
+
+    println(
+        "Персональные данные: \n" +
+                "Фамилия: $userSurName \n" +
+                "Имя: $userName \n" +
+                "Отчество: $userFatherName \n" +
+                "Возраст: $userAge \n"
+
+    )
+
+
+
+
 
 
 }
