86f09efbf1dda37f80d783a87d9b9b337b0c6e2b
grinsva <grinsva@gmail.com>
2024-06-12 15:46:01 +0300

    KS 4-3  ДОбавил константы погоды и тента

--- src/main/kotlin/lesson_4/less_4_task_3.kt
+++ src/main/kotlin/lesson_4/less_4_task_3.kt
@@ -2,16 +2,20 @@ package lesson_4
 
 const val HUMIDITY_GOOD = 20
 const val SEASON_NOT_GOOD = "зима"
+const val TENT_IS_OPEN = true
+const val TENT_IS_CLOSED = false
+const val WEATHER_SUNNY = true
+const val WEATHER_CLOUDLY = false
 
 fun main() {
 
-    val weatherIsSunny= true
-    val tentIsOpen = true
+    val weatherSunCondition = WEATHER_SUNNY
+    val tentCondition = TENT_IS_OPEN
     val humidity = 20
     val season = "зима"
 
-    val isConditionsGood = weatherIsSunny
-                        && tentIsOpen
+    val isConditionsGood = weatherSunCondition
+                        && tentCondition
                         && ( humidity == HUMIDITY_GOOD )
                         && ( season != SEASON_NOT_GOOD )
 
