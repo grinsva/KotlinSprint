5f9434ebbe514c01171a5aa524a19ac11b83dcf7
grinsva <grinsva@gmail.com>
2024-06-12 15:56:33 +0300

    KS 4-4  переименовал константы

--- src/main/kotlin/lesson_4/less_4_task_4.kt
+++ src/main/kotlin/lesson_4/less_4_task_4.kt
@@ -1,17 +1,19 @@
 package lesson_4
 
-const val DELITEL_2_PROVERKI_4ETNOSTI = 2
-const val ZERO_OSTATOK_PROVERKI_4ETNOSTI = 0
+const val NUMBER_TWO = 2
+const val ZERO = 0
 
 fun main() {
 
     val dayOfPractice = 5
+    val balanceDivision = dayOfPractice % NUMBER_TWO
 
-    val armPractice:   Boolean = ( dayOfPractice % DELITEL_2_PROVERKI_4ETNOSTI > ZERO_OSTATOK_PROVERKI_4ETNOSTI )
-    val bellyPractice: Boolean = ( dayOfPractice % DELITEL_2_PROVERKI_4ETNOSTI > ZERO_OSTATOK_PROVERKI_4ETNOSTI )
 
-    val legPractice:   Boolean = ( dayOfPractice % DELITEL_2_PROVERKI_4ETNOSTI == ZERO_OSTATOK_PROVERKI_4ETNOSTI )
-    val backPractice:  Boolean = ( dayOfPractice % DELITEL_2_PROVERKI_4ETNOSTI == ZERO_OSTATOK_PROVERKI_4ETNOSTI )
+    val armPractice:   Boolean = balanceDivision > ZERO
+    val bellyPractice: Boolean = balanceDivision > ZERO
+
+    val legPractice:   Boolean = balanceDivision == ZERO
+    val backPractice:  Boolean = balanceDivision == ZERO
 
     println("""
         Сегодня $dayOfPractice день тренировок.
