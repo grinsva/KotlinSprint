fe13f1b108e0a5780b141aa240fd06ed0a573d40
grinsva <grinsva@gmail.com>
2024-06-13 10:07:31 +0300

    KS 5-2 отформатировал

--- src/main/kotlin/lesson_5/less_5_task_2.kt
+++ src/main/kotlin/lesson_5/less_5_task_2.kt
@@ -1,15 +1,19 @@
 package lesson_5
 
 const val ACCESS_AGE = 18
-const val PRESENT_YEAR = 2024
 
 fun main() {
+
+    val yearPresent = 2024
 
     println("Введите ваш год рождения:")
     val userBirthYear = readln().toInt()
 
-    val userAge = PRESENT_YEAR - userBirthYear
+    val userAge = yearPresent - userBirthYear
 
-    if ( userAge >= ACCESS_AGE ) println("Показывать экран со скрытым контентом")
-    else println("Вернуться на главный экран")
+    if (userAge >= ACCESS_AGE) {
+        println("Показывать экран со скрытым контентом")
+    } else {
+        println("Вернуться на главный экран")
+    }
 }
