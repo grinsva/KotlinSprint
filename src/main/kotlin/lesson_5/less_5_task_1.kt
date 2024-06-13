1683a1fe03b4959f5b777fdbb862a122504d5f02
grinsva <grinsva@gmail.com>
2024-06-13 10:00:10 +0300

    KS 5-1 отформатировал

--- src/main/kotlin/lesson_5/less_5_task_1.kt
+++ src/main/kotlin/lesson_5/less_5_task_1.kt
@@ -9,6 +9,9 @@ fun main() {
     println("Введите результат суммы числа $a и $b:")
     val inputNum = readln().toInt()
 
-    if (result == inputNum) println("Добро пожаловать!")
-    else println("Доступ запрещен!")
+    if (result == inputNum) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен!")
+    }
 }
