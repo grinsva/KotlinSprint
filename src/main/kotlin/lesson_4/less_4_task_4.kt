30d450b9d045c63cf0e079395daa473e71aec459
grinsva <grinsva@gmail.com>
2024-06-13 09:55:37 +0300

    KS 4-4 отформатировал

--- src/main/kotlin/lesson_4/less_4_task_4.kt
+++ src/main/kotlin/lesson_4/less_4_task_4.kt
@@ -8,18 +8,19 @@ fun main() {
     val dayOfPractice = 5
     val balanceDivision = dayOfPractice % NUMBER_TWO
 
-
-    val armPractice:   Boolean = balanceDivision > ZERO
+    val armPractice: Boolean = balanceDivision > ZERO
     val bellyPractice: Boolean = balanceDivision > ZERO
 
-    val legPractice:   Boolean = balanceDivision == ZERO
-    val backPractice:  Boolean = balanceDivision == ZERO
+    val legPractice: Boolean = balanceDivision == ZERO
+    val backPractice: Boolean = balanceDivision == ZERO
 
-    println("""
+    println(
+        """
         Сегодня $dayOfPractice день тренировок.
         Упражнения для рук: $armPractice  
         Упражнения для ног: $legPractice 
         Упражнения для спины: $backPractice
         Упражнения для пресса: $bellyPractice
-    """.trimIndent())
+    """.trimIndent()
+    )
 }
